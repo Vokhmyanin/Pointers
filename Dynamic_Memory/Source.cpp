410c4d295960d1047e6dcc899b5fb3a14ce56d0b
Евгений <@LAPTOP-IEGUVAC3>
2023-07-25 14:41:17 +0700

    Dynamic_Memoty_DONE

--- Dynamic_Memory/Source.cpp
+++ Dynamic_Memory/Source.cpp
@@ -6,22 +6,42 @@ void Print(int arr[], const int n);
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n; //количетво элементов массива
+	int n; 
 	cout << "Ведите размер массива: "; cin >> n;
 	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	int* buffer = new int[n + 1];
 	for (int i = 0; i < n; i++)
 	{
-		*(arr + i) = rand() % 100;//обращение к элементам массива через арифметику указателейи и оператор разимерования
-
-	cout << arr[i] << "\t";
+		buffer[i] = arr[i];
 	}
-	cout << endl;
 	delete[] arr;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	Print(arr, n);
+	
+	
 }
 void FillRand(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		*(arr + 1) = rand() % 100;
+		arr[i] = rand() % 100;
 	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		
+		cout << arr[i] <<"\t";
+		
+	}
+	cout << endl;
 }
